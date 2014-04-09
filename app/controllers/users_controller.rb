6777baf9d12ddf2c170928cb1a28fb30ac9a04aa
khiangte <bmasseu@gmail.com>
2014-04-09 01:22:15 -0400

    signup login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
 	def signup
+	end
 
+	def save
+		@user = User.new(params[:user])
+		@user.save
+		session[:user] = @user
+		redirect_to root_path
+	end
+
+	def login
+
+	end
+
+	def authenticate
+		@user = User.find_by_user_name(params[:user]["user_name"])
+		if @user[:password] == params[:user]["password"]
+			session[:user] = @user
+			redirect_to root_path
+		else
+			redirect_to login_path
+		end
+	end
+
+	def logout
+		session[:user] = nil
+		redirect_to root_path
 	end
 end
