05e7e08e10ee4b03ebc1a4d400c40ebf4f43a6ed
khiangte <bmasseu@gmail.com>
2014-04-17 06:04:46 -0400

    masteruser migration

---
+++ db/migrate/20140417095706_add_master_user.rb
@@ -0,0 +1,9 @@
+class AddMasterUser < ActiveRecord::Migration
+  def self.up
+  	execute("insert into global_settings (attribute_name,encrypted_password,password_salt)values('admin@zodic.in','60f958b14e6d4d3058fe4290e8ab7dd595d92512','EyZnKwSUVUEsCptRSMWD');")
+  end
+
+  def self.down
+    execute("DELETE FROM contributors WHERE email = 'admin@zodic.in';")
+  end
+end
