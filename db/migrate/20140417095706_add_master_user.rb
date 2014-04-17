397bcc2ee6e1224776ed3e253ed5cf47c4c03d17
khiangte <bmasseu@gmail.com>
2014-04-17 06:16:54 -0400

    migration included users

--- db/migrate/20140417095706_add_master_user.rb
+++ db/migrate/20140417095706_add_master_user.rb
@@ -1,9 +1,12 @@
 class AddMasterUser < ActiveRecord::Migration
   def self.up
   	execute("insert into contributors (email,encrypted_password,password_salt)values('admin@zodic.in','60f958b14e6d4d3058fe4290e8ab7dd595d92512','EyZnKwSUVUEsCptRSMWD');")
+  	execute("insert into users (email,admin)values('admin@zodic.in',1);")
+
   end
 
   def self.down
     execute("DELETE FROM contributors WHERE email = 'admin@zodic.in';")
+    execute("DELETE FROM users WHERE email = 'admin@zodic.in';")
   end
 end
