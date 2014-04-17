67f3228d3b361358f5fede19b16310d1133f64d1
khiangte <bmasseu@gmail.com>
2014-04-17 06:13:13 -0400

    migration fised

--- db/migrate/20140417095706_add_master_user.rb
+++ db/migrate/20140417095706_add_master_user.rb
@@ -1,6 +1,6 @@
 class AddMasterUser < ActiveRecord::Migration
   def self.up
-  	execute("insert into global_settings (attribute_name,encrypted_password,password_salt)values('admin@zodic.in','60f958b14e6d4d3058fe4290e8ab7dd595d92512','EyZnKwSUVUEsCptRSMWD');")
+  	execute("insert into contributors (email,encrypted_password,password_salt)values('admin@zodic.in','60f958b14e6d4d3058fe4290e8ab7dd595d92512','EyZnKwSUVUEsCptRSMWD');")
   end
 
   def self.down
